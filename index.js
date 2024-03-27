4d0445e161c0a6f46a749b02f275783b724765d0
shukri <didashukri0@gmail.com>
2024-03-27 15:43:50 +0300

    added a file

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+const destructivelyAppendCat = () => {
+    cats.push("Ralph")
+}
+
+const destructivelyPrependCat = () => {
+    cats.unshift("Bob")
+}
+
+const destructivelyRemoveLastCat = () => {
+    cats.pop()
+}
+
+const destructivelyRemoveFirstCat = () => {
+    cats.shift()
+}
+
+const appendCat = () => {
+    return [...cats,"Broom"]
+}
+
+const prependCat = () => {
+    return ["Arnold", ...cats]
+}
+
+const removeLastCat = () => {
+    return cats.slice(0,2)
+}
+
+const removeFirstCat = () => {
+    return cats.slice(1,3)
+}
